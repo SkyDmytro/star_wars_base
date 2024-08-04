5fdc626fa82c4efc4e77a826309d17354519cb12
Dima <skydmytro@gmail.com>
2024-08-04 19:24:49 +0200

    hotfix: small fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { CharactersListPage } from "./pages/CharactersListPage.tsx";
+import { CharacterDetailsPage } from "./pages/CharacterDetailsPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/characters",
     element: <CharactersListPage />,
+  },
+  {
+    path: "/characters/:id",
+    element: <CharacterDetailsPage />,
   },
 ]);
 

fa8f7fd0ae4f436a3e7df9a92271ee59f041a192
Dima <skydmytro@gmail.com>
2024-08-08 19:54:05 +0200

    feat: change main path

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 import { CharactersListPage } from './pages/CharactersListPage';
 import { CharacterDetailsPage } from './pages/CharacterDetailsPage';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/characters" />,
+  },
   {
     path: '/characters',
     element: <CharactersListPage />,
